15cd277ac168ad9ab6b36c7c4df406d4515bd5f7
gbisra <GurvirBisra@Gurvirs-MacBook-Pro.local>
2016-02-16 20:29:11 -0800

    update message

--- cont/message.php
+++ cont/message.php
@@ -6,12 +6,9 @@ if($_POST['method'] == "insert"){
     insert_message();
 }
 
-if($_POST['method'] == "getAll"){
+if($_POST['method'] == "getall"){
     get_messages();
 }
 
-if($_POST['method'] == "like"){
-    insert_like();
-}
 
 ?>
--- model/msgdb.php
+++ model/msgdb.php
@@ -5,18 +5,16 @@
         //insert user into the db users table
         global $db;
         
-        $query = "INSERT INTO messages (message, user_id) VALUES ('".$_POST['message']."', '".$_POST['user_id']."')";
+        $query = "INSERT INTO messages (message, user_id) VALUES ('".$_POST['message']."', ".$_POST['user_id'].")";
         $result = $db->query($query);
     }
     
     function get_messages(){
         //read the users from the db users table
         global $db;
-        $query = "SELECT messages.id AS message_id, messages.message, users.username, messages.user_id,
+        $query = "SELECT * FROM messages
+        LEFT JOIN users ON users.id = messages.user_id";
         
-                (SELECT (*) FROM likes WHERE likes.message_id = message_id) AS likeNum  
-                FROM messages 
-                LEFT JOIN users ON users.id = messages.user_id";
         $result = $db->query($query);
         echo json_encode($result->fetchAll());
     }
@@ -29,12 +27,8 @@
         //remove a row o fuser from the db users table
     }
     
-    function insert_like(){
-        global $db;
-        $query = "INSERT INTO likes ()message_id, user_id) VALUES ('".$_POST['message_id']."' , "._POST['user_id'].")";
-        $result = $db->query($query);
-        echo json_encode($result->fetchAll());
-    }
+    
+    
     //Testing
     //insert_user();
 ?>
